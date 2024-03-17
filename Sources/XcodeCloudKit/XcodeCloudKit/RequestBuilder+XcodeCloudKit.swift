4bb5ebaf76e0d9ef4cec78d3351236bf261473f3
Pol Piella Abadia <hi@polpiella.dev>
2024-03-17 10:50:32 +0100

    Gets single repository by id

--- Sources/XcodeCloudKit/XcodeCloudKit/RequestBuilder+XcodeCloudKit.swift
+++ Sources/XcodeCloudKit/XcodeCloudKit/RequestBuilder+XcodeCloudKit.swift
@@ -9,4 +9,14 @@ extension RequestBuilder {
         
         return .init(path: productsEndpoint.path, method: productsEndpoint.method, queryParameters: productsEndpoint.query)
     }
+    
+    static func product(with id: String) -> TransportRequest<CiProductResponse> {
+        let productEndpoint = APIEndpoint
+            .v1
+            .ciProducts
+            .id(id)
+            .get(parameters: .init(include: [.primaryRepositories]))
+        
+        return .init(path: productEndpoint.path, method: productEndpoint.method, queryParameters: productEndpoint.query)
+    }
 }
--- Sources/XcodeCloudKit/XcodeCloudKit/XcodeCloudKit.swift
+++ Sources/XcodeCloudKit/XcodeCloudKit/XcodeCloudKit.swift
@@ -1,6 +1,7 @@
 public protocol XcodeCloudKit {
     func allProducts() async throws -> [Product]
     func product(withRepositoryName repository: String) async throws -> Product?
+    func product(withId id: String) async throws -> Product?
 }
 
 final class DefaultXcodeCloudKit {
@@ -41,5 +42,26 @@ extension DefaultXcodeCloudKit: XcodeCloudKit {
         let allProducts = try await allProducts()
         
         return allProducts.first(where: { $0.repository.name == repository })
+    }
+    
+    public func product(withId id: String) async throws -> Product? {
+        let productResponse = try await transport.perform(request: RequestBuilder.product(with: id))
+        
+        let repositories: [(id: String, name: String)] = productResponse.included?.compactMap { includedItem in
+            if case .scmRepository(let scmData) = includedItem, let name = scmData.attributes?.repositoryName {
+                return (scmData.id, name)
+            }
+            
+            return nil
+        } ?? []
+        
+        let product = productResponse.data
+        
+        if let repository = repositories.first(where: { product.relationships?.primaryRepositories?.data?.first?.id == $0.id }),
+           let name = product.attributes?.name {
+            return Product(name: name, id: product.id, repository: .init(id: repository.id, name: repository.name))
+        } else {
+            return nil
+        }
     }
 }
