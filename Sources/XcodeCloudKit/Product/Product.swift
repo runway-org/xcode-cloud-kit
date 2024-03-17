351ac5dff194fea3d4a3bbb057b3f18b04926f31
Pol Piella Abadia <hi@polpiella.dev>
2024-03-17 11:04:31 +0100

    Adds methods to fetch list of workflows and single workflows

--- Sources/XcodeCloudKit/Product/Product.swift
+++ Sources/XcodeCloudKit/Product/Product.swift
@@ -8,4 +8,23 @@ public struct Product {
         public let id: String
         public let name: String
     }
+    
+    public func workflows() async throws -> [Workflow] {
+        let allWorkflows = try await transport.perform(request: RequestBuilder.allWorkflows(for: id))
+        
+        return allWorkflows.data.map { Workflow(id: $0.id, name: $0.attributes.name, transport: transport) }
+    }
+    
+    public func workflow(withId id: String) async throws -> Workflow {
+        let workflow = try await transport.perform(request: RequestBuilder.workflow(with: id))
+        
+        return Workflow(id: workflow.data.id, name: workflow.data.attributes?.name ?? "", transport: transport)
+    }
+    
+    public func workflow(withName name: String) async throws -> Workflow? {
+        try await transport.perform(request: RequestBuilder.allWorkflows(for: id))
+            .data
+            .map { Workflow(id: $0.id, name: $0.attributes.name, transport: transport) }
+            .first(where: { $0.name == name })
+    }
 }
---
+++ Sources/XcodeCloudKit/Product/RequestBuilder+Product.swift
@@ -0,0 +1,36 @@
+import AppStoreConnect_Swift_SDK
+
+extension RequestBuilder {
+    struct WorkflowsResponse: Decodable {
+        let data: [Data]
+        
+        struct Data: Decodable {
+            let id: String
+            let attributes: Attributes
+            
+            struct Attributes: Decodable {
+                let name: String
+            }
+        }
+    }
+    
+    static func allWorkflows(for productId: String) -> TransportRequest<WorkflowsResponse> {
+        let allWorkflowsEndpoint = APIEndpoint
+            .v1
+            .ciProducts
+            .id(productId)
+            .workflows
+        
+        return .init(path: "GET", method: allWorkflowsEndpoint.path, queryParameters: [("fields[ciWorkflows]", "name")])
+    }
+    
+    static func workflow(with id: String) -> TransportRequest<CiWorkflowResponse> {
+        let workflow = APIEndpoint
+            .v1
+            .ciWorkflows
+            .id(id)
+            .get()
+        
+        return .init(path: workflow.path, method: workflow.method, queryParameters: workflow.query)
+    }
+}
